9ef5506da086f7d88114a8aef336b1efefbf29da
chidera-nwankwo <cnwankwo1234@yahoo.com>
2024-04-20 14:50:36 -0500

    implemented fetchMatches (IDs)

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,18 +1,22 @@
 
 //fetches match history
-async function fetchMatches(summonerName, riotTagLine, region) {
-    const variable1 = summonerName;
-    const variable2 = riotTagLine;
-    const variable3 = region;
+async function fetchMatches(puuid, region) {
+    puuid = 'MITT5QcsTavDlKKXwrxuwVrWlRNKkJy3HSQp7IdmbhPSN2SLSl8HmFOZzbiqHs0m4MQc37MmKV5asg';
 
-    console.log(region)
-
-    const response = await fetch('https://w49d8ezvz3.execute-api.us-east-2.amazonaws.com/rgapi/summoner/region/id/tft/matches');
+    const response = await fetch('https://w49d8ezvz3.execute-api.us-east-2.amazonaws.com/rgapi/summoner/region/id/tft/matches?region=' + region + '&puuid=' + puuid);
     const data = await response.json();
     
     console.log(data);
 }
 
+//fetches puuid
+async function fetchPuuid(summonerName, riotTagLine, region) {
+
+    const response = await fetch('https://w49d8ezvz3.execute-api.us-east-2.amazonaws.com/rgapi/summoner/region/id?summonerName=' + summonerName + '&riotTagLine=' + riotTagLine + '&region=' + region);
+    const data = await response.json();
+    
+    console.log(data);
+}
 
 // gets user input for summoner name and riot tag and region
 document.querySelector('form').addEventListener('submit', function(event) {
@@ -22,6 +26,6 @@ document.querySelector('form').addEventListener('submit', function(event) {
     var region = document.getElementById('region').value;
     console.log(summonerName, riotTagLine, region);
 
-    fetchMatches(summonerName,riotTagLine,region);
+    fetchPuuid(summonerName,riotTagLine, region);
     
 });
