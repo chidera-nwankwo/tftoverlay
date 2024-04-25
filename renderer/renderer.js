52bbf4059df641fa20bf09ed4d520bf99e5cb091
chidera-nwankwo <cnwankwo1234@yahoo.com>
2024-04-25 04:31:20 -0500

    implemented puuid request and placements request

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,31 +1,65 @@
+//fetches puuid
+async function fetchPuuid(summonerName, riotTagLine, region) {
 
-//fetches match history
+    const response = await fetch('https://w49d8ezvz3.execute-api.us-east-2.amazonaws.com/rgapi/summoner/region/id?region=' + region + '&summonerName=' + summonerName + '&tagline=' + riotTagLine);
+    const data = await response.json();
+    
+    let puuid = data.body.puuid;
+    //console.log(puuid);
+    return puuid;
+}
+
+// fetches match history ID ONLY
 async function fetchMatches(puuid, region) {
-    puuid = 'MITT5QcsTavDlKKXwrxuwVrWlRNKkJy3HSQp7IdmbhPSN2SLSl8HmFOZzbiqHs0m4MQc37MmKV5asg';
 
     const response = await fetch('https://w49d8ezvz3.execute-api.us-east-2.amazonaws.com/rgapi/summoner/region/id/tft/matches?region=' + region + '&puuid=' + puuid);
     const data = await response.json();
     
-    console.log(data);
+    let parsedMatches = data.body;
+    //console.log(parsedMatches);
+    return parsedMatches;
 }
 
-//fetches puuid
-async function fetchPuuid(summonerName, riotTagLine, region) {
+// fetches match details (placement)
+async function fetchMatchDetails(matchIDArray, region, puuid) {
 
-    const response = await fetch('https://w49d8ezvz3.execute-api.us-east-2.amazonaws.com/rgapi/summoner/region/id?summonerName=' + summonerName + '&riotTagLine=' + riotTagLine + '&region=' + region);
-    const data = await response.json();
+    let placementArray = [];
+
+    for (let i=0; i< 20; i++) {
+        const response = await fetch('https://w49d8ezvz3.execute-api.us-east-2.amazonaws.com/rgapi/summoner/region/id/tft/matches/details?matchID=' + matchIDArray[i] + '&region=' + region);
+        const data = await response.json();
+        
+        // return the index of the current user
+        let participants = data.body.metadata.participants;
+        let index = participants.indexOf(puuid);
     
-    console.log(data);
+        let placement = data.body.info.participants[index].placement;
+
+        placementArray.push(placement)
+
+    }
+
+    return placementArray;
 }
 
 // gets user input for summoner name and riot tag and region
 document.querySelector('form').addEventListener('submit', function(event) {
+
+    // prevents reloading of console window upon form submit
     event.preventDefault();
+
     var summonerName = document.getElementById('summonerName').value;
     var riotTagLine = document.getElementById('tagLine').value;
     var region = document.getElementById('region').value;
-    console.log(summonerName, riotTagLine, region);
 
-    fetchPuuid(summonerName,riotTagLine, region);
+    //console.log(summonerName, riotTagLine, region);
+
+    // chaining promises so the functions execute in order
+    fetchPuuid(summonerName, riotTagLine, region)
+        .then((puuid) => fetchMatches(puuid,region)
+        .then((matchhistory) => fetchMatchDetails(matchhistory, region, puuid)))
+        .then((placementArray) => {
+            console.log('placement: ' + placementArray);
+        })
     
 });
